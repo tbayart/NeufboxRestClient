040100ee95bcc8f954d85bc0e3216f34d28fdc9f
tbayart <tbayart@nordnet.fr>
2024-02-23 19:04:53 +0100

    Added StringHelpers.cs and unit tests

---
+++ src/NeufboxRestClient/StringHelpers.cs
@@ -0,0 +1,71 @@
+﻿namespace Toolbox
+{
+    /// <summary>
+    /// Helper methods to manipulate strings.
+    /// </summary>
+    public static class StringHelpers
+    {
+        #region hexadecimal convertion
+        /// <summary>
+        /// Used to convert a numeric value to its lower case character representation.
+        /// </summary>
+        private static string hexTableLowerCase = "0123456789abcdef";
+
+        /// <summary>
+        /// Used to convert a numeric value to its upper case character representation.
+        /// </summary>
+        private static string hexTableUpperCase = "0123456789ABCDEF";
+
+        /// <summary>
+        /// Used to convert a character to its numeric value.
+        /// </summary>
+        private static Dictionary<char, byte> charToByte = new Dictionary<char, byte> {
+            {'0', 0x00 }, {'1', 0x01 }, {'2', 0x02 }, {'3', 0x03 },
+            {'4', 0x04 }, {'5', 0x05 }, {'6', 0x06 }, {'7', 0x07 },
+            {'8', 0x08 }, {'9', 0x09 }, {'a', 0x0a }, {'b', 0x0b },
+            {'c', 0x0c }, {'d', 0x0d }, {'e', 0x0e }, {'f', 0x0f },
+            {'A', 0X0A }, {'B', 0X0B }, {'C', 0X0c }, {'D', 0X0D },
+            {'E', 0X0E }, {'F', 0X0F }
+        };
+
+        /// <summary>
+        /// Convert a string containing HEX values to a byte array
+        /// </summary>
+        /// <param name="source">The string containing HEX values</param>
+        /// <returns>The resulting byte array</returns>
+        public static byte[] HexToArray(this string source)
+        {
+            var result = new byte[source.Length >> 1];
+            var indexSource = 0;
+            var indexResult = 0;
+            while (indexSource < source.Length)
+            {
+                var value = charToByte[source[indexSource++]];
+                value <<= 4;
+                value += charToByte[source[indexSource++]];
+                result[indexResult++] = value;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Convert a byte array to a string containing HEX values
+        /// </summary>
+        /// <param name="source">The byte array</param>
+        /// <param name="toUpperCase">True to get an upper case output, false otherwise.</param>
+        /// <returns>The resulting string containing HEX values</returns>
+        public static string ArrayToHex(this byte[] source, bool toUpperCase = false)
+        {
+            var hexTable = toUpperCase ? hexTableUpperCase : hexTableLowerCase;
+            var hex = new char[source.Length * 2];
+            var index = 0;
+            foreach (byte b in source)
+            {
+                hex[index++] = hexTable[b >> 4];
+                hex[index++] = hexTable[b & 0x0F];
+            }
+            return new string(hex);
+        }
+        #endregion hexadecimal convertion
+    }
+}
---
+++ src/UnitTests/StringHelpersTests.cs
@@ -0,0 +1,67 @@
+using FluentAssertions;
+using Toolbox;
+
+namespace UnitTests
+{
+    /// <summary>
+    /// Tests related to <see cref="StringHelpers"/> class.
+    /// </summary>
+    public class StringHelpersTests
+    {
+        /// <summary>
+        /// Byte array version of data.
+        /// </summary>
+        public static readonly byte[] expectedResultHexArray = [
+            0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b,
+            0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x21, 0x32, 0x43, 0x54, 0x65, 0x76, 0x87,
+            0x98, 0xa9, 0xba, 0xcb, 0xdc, 0xed, 0xfe, 0xff, 0xee, 0xdd, 0xcc, 0xbb,
+            0xaa, 0x99, 0x88, 0x77, 0x66, 0x55, 0x44, 0x33, 0x22, 0x11, 0x00, 0xff
+        ];
+
+        /// <summary>
+        /// String representation of previous byte array.
+        /// </summary>
+        public static string expectedResultHexString =
+            "000102030405060708090a0b0c0d0e0f102132435465768798a9bacbdcedfeffeeddccbbaa99887766554433221100ff";
+
+        /// <summary>
+        /// Unit test for <see cref="StringHelpers.ArrayToHex"/> with default lower casing.
+        /// </summary>
+        [Fact]
+        public void ArrayToHexTest()
+        {
+            var result = expectedResultHexArray.ArrayToHex();
+            result.Should().Be(expectedResultHexString);
+        }
+
+        /// <summary>
+        /// Unit test for <see cref="StringHelpers.HexToArray"/> with default lower casing.
+        /// </summary>
+        [Fact]
+        public void HexToArrayTest()
+        {
+            var result = expectedResultHexString.HexToArray();
+            result.Should().BeEquivalentTo(expectedResultHexArray, options => options.WithStrictOrdering());
+        }
+
+        /// <summary>
+        /// Unit test for <see cref="StringHelpers.ArrayToHex"/> with upper casing.
+        /// </summary>
+        [Fact]
+        public void ArrayToHexUpperCaseTest()
+        {
+            var result = expectedResultHexArray.ArrayToHex(true);
+            result.Should().Be(expectedResultHexString.ToUpper());
+        }
+
+        /// <summary>
+        /// Unit test for <see cref="StringHelpers.HexToArray"/> with upper casing.
+        /// </summary>
+        [Fact]
+        public void HexToArrayUpperCaseTest()
+        {
+            var result = expectedResultHexString.ToUpper().HexToArray();
+            result.Should().BeEquivalentTo(expectedResultHexArray, options => options.WithStrictOrdering());
+        }
+    }
+}
