642a4dcb72ccbb7d4ac7f8ce5bad13ae8b60f0d1
tbayart <tbayart@nordnet.fr>
2024-03-04 00:03:11 +0100

    Added NeufboxRestClient/Models/NeufboxSettings.cs

---
+++ src/NeufboxRestClient/Models/NeufboxSettings.cs
@@ -0,0 +1,19 @@
+﻿namespace NeufboxRestClient.Models
+{
+    /// <summary>
+    /// Used to store the <see cref="NeufboxClient"/> settings.
+    /// </summary>
+    public class NeufboxSettings
+    {
+        /// <summary>
+        /// The box's default host.
+        /// </summary>
+        public const string defaultHost = "192.168.1.1";
+
+        /// <summary>
+        /// The host or ip address of the box.
+        /// </summary>
+        /// <example></example>
+        public string Host { get; set; } = defaultHost;
+    }
+}
