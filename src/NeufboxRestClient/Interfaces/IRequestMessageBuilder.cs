19c7d1cb3a29f558d59fe210c442074509d4861f
tbayart <tbayart@nordnet.fr>
2024-03-04 23:24:03 +0100

    Added IRequestMessageBuilder interface

---
+++ src/NeufboxRestClient/Interfaces/IRequestMessageBuilder.cs
@@ -0,0 +1,16 @@
+﻿namespace NeufboxRestClient.Interfaces
+{
+    /// <summary>
+    /// Provide <see cref="HttpRequestMessage"/> generation.
+    /// </summary>
+    public interface IRequestMessageBuilder
+    {
+        /// <summary>
+        /// Build a <see cref="HttpRequestMessage"/> instance from <paramref name="request"/>.
+        /// </summary>
+        /// <typeparam name="TRequest">The request type.</typeparam>
+        /// <param name="request">The request instance.</param>
+        /// <returns>A <see cref="HttpRequestMessage"/> instance.</returns>
+        HttpRequestMessage Build<TRequest>(TRequest request);
+    }
+}
