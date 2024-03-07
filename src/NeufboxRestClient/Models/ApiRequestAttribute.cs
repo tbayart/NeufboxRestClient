7e8c58779446850f5badb95e52b63b841fad3c8b
tbayart <tbayart@nordnet.fr>
2024-03-07 23:51:02 +0100

    Added ApiRequestAttribute to describe API requests

---
+++ src/NeufboxRestClient/Models/ApiRequestAttribute.cs
@@ -0,0 +1,30 @@
+﻿namespace NeufboxRestClient.Models
+{
+    /// <summary>
+    /// Attribute used to describe a API request.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ApiRequestAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new <see cref="ApiRequestAttribute"/> instance.
+        /// </summary>
+        /// <param name="apiMethod"></param>
+        /// <param name="httpVerb"></param>
+        public ApiRequestAttribute(string apiMethod, HttpVerb httpVerb)
+        {
+            ApiMethod = apiMethod;
+            HttpVerb = httpVerb;
+        }
+
+        /// <summary>
+        /// The API method from the API specifications.
+        /// </summary>
+        public string ApiMethod { get; init; }
+
+        /// <summary>
+        /// The http verb to use for this API method.
+        /// </summary>
+        public HttpVerb HttpVerb { get; init; }
+    }
+}
