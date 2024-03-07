033e0736f2a55e79be7e059d9c52da0cee14bcb1
tbayart <tbayart@nordnet.fr>
2024-03-07 23:39:34 +0100

    Added StatusCode, enum from API returned status codes

---
+++ src/NeufboxRestClient/Models/StatusCode.cs
@@ -0,0 +1,22 @@
+﻿using System.Xml.Serialization;
+
+namespace NeufboxRestClient.Models
+{
+    /// <summary>
+    /// The status codes returned by the API.
+    /// </summary>
+    public enum StatusCode
+    {
+        /// <summary>
+        /// Success status code returned when the request is successful.
+        /// </summary>
+        [XmlEnum("ok")]
+        Success,
+
+        /// <summary>
+        /// Failed status code returned when the request is not successful.
+        /// </summary>
+        [XmlEnum("fail")]
+        Failed,
+    }
+}
