659a3e45f332a3fb9b510ee92fd413c08de83967
tbayart <tbayart@nordnet.fr>
2024-03-08 20:12:22 +0100

    Added AuthGetToken response model for auth.getToken api method

---
+++ src/NeufboxRestClient/Models/Responses/AuthGetToken.cs
@@ -0,0 +1,47 @@
+﻿using System.Xml.Serialization;
+
+namespace NeufboxRestClient.Models.Responses
+{
+    /// <summary>
+    /// Response to <see cref="Requests.AuthGetToken"/>.
+    /// </summary>
+    [XmlRoot("auth")]
+    public class AuthGetToken
+    {
+        /// <summary>
+        /// The token provided by the box.
+        /// </summary>
+        [XmlAttribute("token")]
+        public string Token { get; set; }
+
+        /// <summary>
+        /// The authentication method.
+        /// </summary>
+        [XmlAttribute("method")]
+        public AuthMethod Method { get; set; }
+    }
+
+    /// <summary>
+    /// The known authentication methods.
+    /// </summary>
+    public enum AuthMethod
+    {
+        /// <summary>
+        /// Authentication require password.
+        /// </summary>
+        [XmlEnum("passwd")]
+        Password,
+
+        /// <summary>
+        /// Authentication require physically pushing the WPS button of the box.
+        /// </summary>
+        [XmlEnum("button")]
+        Button,
+
+        /// <summary>
+        /// Authentication can be done using <see cref="Password"/> or <see cref="Button"/> scenario.
+        /// </summary>
+        [XmlEnum("all")]
+        All,
+    }
+}
