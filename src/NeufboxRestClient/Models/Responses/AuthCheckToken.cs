3373ea4274c69affe5ebdd7185699355a0beb40b
tbayart <tbayart@nordnet.fr>
2024-03-08 20:13:31 +0100

    Added AuthCheckToken response model for auth.checkToken api method

---
+++ src/NeufboxRestClient/Models/Responses/AuthCheckToken.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace NeufboxRestClient.Models.Responses
+{
+    /// <summary>
+    /// Response to <see cref="Requests.AuthCheckToken"/>.
+    /// </summary>
+    [XmlRoot("auth")]
+    public class AuthCheckToken
+    {
+        /// <summary>
+        /// The validated authentication token.
+        /// </summary>
+        [XmlAttribute("token")]
+        public string Token { get; set; }
+    }
+}
