fe24c00070abafc3ce03a96d5b009900e255587e
tbayart <tbayart@nordnet.fr>
2024-03-08 20:12:59 +0100

    Added AuthCheckToken model for auth.checkToken request

---
+++ src/NeufboxRestClient/Models/Requests/AuthCheckToken.cs
@@ -0,0 +1,21 @@
+﻿using NeufboxRestClient.Constants;
+
+namespace NeufboxRestClient.Models.Requests
+{
+    /// <summary>
+    /// auth.checkToken request.
+    /// </summary>
+    [ApiRequest(ApiMethod.AuthCheckToken, HttpVerb.GET)]
+    public class AuthCheckToken
+    {
+        /// <summary>
+        /// The token provided by <see cref="AuthGetToken"/> request.
+        /// </summary>
+        public string Token { get; set; }
+
+        /// <summary>
+        /// The hash calculated using a <see cref="Interfaces.IHashProcessor"/> instance.
+        /// </summary>
+        public string Hash { get; set; }
+    }
+}
