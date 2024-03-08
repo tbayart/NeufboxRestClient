a877b2a2fca8d6fffdeb5da3386e0da338c0818b
tbayart <tbayart@nordnet.fr>
2024-03-08 20:11:40 +0100

    Added AuthGetToken model for auth.getToken request

---
+++ src/NeufboxRestClient/Models/Requests/AuthGetToken.cs
@@ -0,0 +1,12 @@
+﻿using NeufboxRestClient.Constants;
+
+namespace NeufboxRestClient.Models.Requests
+{
+    /// <summary>
+    /// auth.getToken request.
+    /// </summary>
+    [ApiRequest(ApiMethod.AuthGetToken, HttpVerb.GET)]
+    public class AuthGetToken
+    {
+    }
+}
