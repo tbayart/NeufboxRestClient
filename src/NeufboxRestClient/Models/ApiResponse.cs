44fb32fa44321b7eccead99602c8e8524c72d8a2
tbayart <tbayart@nordnet.fr>
2024-03-10 00:12:50 +0100

    Added ApiResponse.cs

---
+++ src/NeufboxRestClient/Models/ApiResponse.cs
@@ -0,0 +1,23 @@
+﻿using System.Xml.Serialization;
+
+namespace NeufboxRestClient.Models
+{
+    /// <summary>
+    /// Model used to deserialize API response.
+    /// </summary>
+    [XmlRoot("rsp")]
+    public class ApiResponse
+    {
+        /// <summary>
+        /// The response status.
+        /// </summary>
+        [XmlAttribute("stat")]
+        public StatusCode Status { get; set; }
+
+        /// <summary>
+        /// The API version.
+        /// </summary>
+        [XmlAttribute("version")]
+        public string Version { get; set; }
+    }
+}
