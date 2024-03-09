7e2da3c0261d2f87870fbc740893ee0c7397e134
tbayart <tbayart@nordnet.fr>
2024-03-10 00:12:00 +0100

    Added ApiError.cs

---
+++ src/NeufboxRestClient/Models/ApiError.cs
@@ -0,0 +1,23 @@
+﻿using System.Xml.Serialization;
+
+namespace NeufboxRestClient.Models
+{
+    /// <summary>
+    /// Model used to deserialize API response when an error is returned.
+    /// </summary>
+    [XmlRoot("err")]
+    public class ApiError
+    {
+        /// <summary>
+        /// The error code.
+        /// </summary>
+        [XmlAttribute("code")]
+        public int Code { get; set; }
+
+        /// <summary>
+        /// The error message.
+        /// </summary>
+        [XmlAttribute("message")]
+        public string Message { get; set; }
+    }
+}
