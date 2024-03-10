dfc17d72cb53b357a7a0289fb9d5fdb4548df149
tbayart <tbayart@nordnet.fr>
2024-03-10 23:49:31 +0100

    Added IsPrivate property to ApiRequestAttribute to distinguish public and private API methods.

--- src/NeufboxRestClient/Models/ApiRequestAttribute.cs
+++ src/NeufboxRestClient/Models/ApiRequestAttribute.cs
@@ -3,28 +3,29 @@
     /// <summary>
     /// Attribute used to describe a API request.
     /// </summary>
+    /// <remarks>
+    /// Initializes a new <see cref="ApiRequestAttribute"/> instance.
+    /// </remarks>
+    /// <param name="apiMethod">The API method this model is mapped to.</param>
+    /// <param name="httpVerb">The http verb to use with the method.</param>
+    /// <param name="isPrivate">True if the method is private (require authentication), false otherwise.</param>
     [AttributeUsage(AttributeTargets.Class)]
-    public class ApiRequestAttribute : Attribute
+    public class ApiRequestAttribute(string apiMethod, HttpVerb httpVerb, bool isPrivate) : Attribute
     {
-        /// <summary>
-        /// Initializes a new <see cref="ApiRequestAttribute"/> instance.
-        /// </summary>
-        /// <param name="apiMethod"></param>
-        /// <param name="httpVerb"></param>
-        public ApiRequestAttribute(string apiMethod, HttpVerb httpVerb)
-        {
-            ApiMethod = apiMethod;
-            HttpVerb = httpVerb;
-        }
-
         /// <summary>
         /// The API method from the API specifications.
         /// </summary>
-        public string ApiMethod { get; init; }
+        public string ApiMethod { get; init; } = apiMethod;
 
         /// <summary>
         /// The http verb to use for this API method.
         /// </summary>
-        public HttpVerb HttpVerb { get; init; }
+        public HttpVerb HttpVerb { get; init; } = httpVerb;
+
+        /// <summary>
+        /// True if the method is private, false otherwise.
+        /// A private method require authentication to be performed successfully prior the API method call.
+        /// </summary>
+        public bool IsPrivate { get; init; } = isPrivate;
     }
 }
--- src/NeufboxRestClient/Models/Requests/AuthGetToken.cs
+++ src/NeufboxRestClient/Models/Requests/AuthGetToken.cs
@@ -5,7 +5,7 @@ namespace NeufboxRestClient.Models.Requests
     /// <summary>
     /// auth.getToken request.
     /// </summary>
-    [ApiRequest(ApiMethod.AuthGetToken, HttpVerb.GET)]
+    [ApiRequest(ApiMethod.AuthGetToken, HttpVerb.GET, false)]
     public class AuthGetToken
     {
     }
