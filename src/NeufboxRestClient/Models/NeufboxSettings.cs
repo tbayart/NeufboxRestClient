28f5b256b0a5cbf9b61846e5d6df89587d6f7fad
tbayart <tbayart@nordnet.fr>
2024-03-10 23:48:08 +0100

    Added more settings to NeufboxSettings.cs

--- src/NeufboxRestClient/Models/NeufboxSettings.cs
+++ src/NeufboxRestClient/Models/NeufboxSettings.cs
@@ -6,14 +6,36 @@
     public class NeufboxSettings
     {
         /// <summary>
+        /// The box's default scheme.
+        /// </summary>
+        public const string defaultScheme = "http";
+
+        /// <summary>
         /// The box's default host.
         /// </summary>
         public const string defaultHost = "192.168.1.1";
 
         /// <summary>
-        /// The host or ip address of the box.
+        /// The API's default path.
         /// </summary>
-        /// <example></example>
+        public const string defaultApiPath = "/api/1.0";
+
+        /// <summary>
+        /// The scheme to call the API.
+        /// Defaulted to http
+        /// </summary>
+        public string Scheme { get; set; } = defaultScheme;
+
+        /// <summary>
+        /// The host or ip address of the box.
+        /// Default is 192.168.1.1
+        /// </summary>
         public string Host { get; set; } = defaultHost;
+
+        /// <summary>
+        /// The path to request the API.
+        /// Default is /api/1.0
+        /// </summary>
+        public string ApiPath { get; set; } = defaultApiPath;
     }
 }
