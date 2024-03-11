a108c9deee2e6104aeb65bd576799d750f409afb
tbayart <tbayart@nordnet.fr>
2024-03-11 20:07:44 +0100

    Added IApiResponseDeserializer.cs interface

---
+++ src/NeufboxRestClient/Interfaces/IApiResponseDeserializer.cs
@@ -0,0 +1,15 @@
+﻿using NeufboxRestClient.Models;
+
+namespace NeufboxRestClient.Interfaces
+{
+    public interface IApiResponseDeserializer
+    {
+        /// <summary>
+        /// Deserialize the API response to requested type <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">The result type in the response.</typeparam>
+        /// <param name="apiResponse">The response from the API.</param>
+        /// <returns></returns>
+        ApiResult<T> Deserialize<T>(string apiResponse);
+    }
+}
