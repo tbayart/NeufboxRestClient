8bf183d09f01331c7943f1d66284ed32358f636d
tbayart <tbayart@nordnet.fr>
2024-03-12 22:49:32 +0100

    Added INeufboxClient.cs interface

---
+++ src/NeufboxRestClient/Interfaces/INeufboxClient.cs
@@ -0,0 +1,33 @@
+﻿using NeufboxRestClient.Models;
+
+namespace NeufboxRestClient.Interfaces
+{
+    /// <summary>
+    /// Neufbox client API interface.
+    /// </summary>
+    public interface INeufboxClient : INeufboxClientPublic, INeufboxClientPrivate
+    {
+        /// <summary>
+        /// Login API to authenticate the client and allow access to private APIs.
+        /// </summary>
+        /// <param name="username">The username.</param>
+        /// <param name="password">The password.</param>
+        /// <returns>A task instance to provide the asynchronous Login result.</returns>
+        Task<ApiResult> LoginAsync(string username, string password);
+    }
+
+    /// <summary>
+    /// Neufbox client interface with public APIs accessible without authentication.
+    /// </summary>
+    public interface INeufboxClientPublic
+    {
+    }
+
+    /// <summary>
+    /// Neufbox client interface with private APIs requiring authentication.
+    /// </summary>
+    public interface INeufboxClientPrivate
+    {
+
+    }
+}
