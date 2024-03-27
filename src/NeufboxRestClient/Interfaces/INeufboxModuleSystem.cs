fc2c722d96886afc921c50c76861b337208f270c
tbayart <tbayart@nordnet.fr>
2024-03-27 23:32:08 +0100

    Added INeufboxModuleSystem.cs interface for System module APIs

---
+++ src/NeufboxRestClient/Interfaces/INeufboxModuleSystem.cs
@@ -0,0 +1,45 @@
+﻿using NeufboxRestClient.Models;
+
+namespace NeufboxRestClient.Interfaces
+{
+    /// <summary>
+    /// Neufbox client interface for System related APIs.
+    /// </summary>
+    public interface INeufboxModuleSystem
+    {
+        /// <summary>
+        /// Provide system information.
+        /// </summary>
+        /// <returns>A <see cref="SystemInformation"/> instance containing all information.</returns>
+        Task<SystemInformation> GetInformationAsync();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <exception cref="UnauthorizedException" />
+        /// <returns></returns>
+        Task<NetworkInterface[]> GetInterfaceListAsync();
+
+        /// <summary>
+        /// Get the WPA key.
+        /// </summary>
+        /// <exception cref="UnauthorizedException" />
+        Task<string> GetWpaKeyAsync();
+
+        /// <summary>
+        /// Reboot the box.
+        /// </summary>
+        /// <exception cref="UnauthorizedException" />
+        Task RebootAsync();
+
+        /// <summary>
+        /// Change the network mode of the box.
+        /// </summary>
+        Task SetNetworkModeAsync(NetworkMode networkMode);
+
+        /// <summary>
+        /// Set the client reference.
+        /// </summary>
+        Task<string> SetClientReferenceAsync(string clientReference);
+    }
+}
