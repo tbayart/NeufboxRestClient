a28973da4ea3bb9a9ae92c27c26080e7469de5d8
tbayart <tbayart@nordnet.fr>
2024-03-27 23:36:14 +0100

    Added SystemGetInfo.cs request and response API models

---
+++ src/NeufboxRestClient/Internal/Models/Requests/SystemGetInfo.cs
@@ -0,0 +1,12 @@
+﻿using NeufboxRestClient.Constants;
+
+namespace NeufboxRestClient.Internal.Models.Requests
+{
+    /// <summary>
+    /// system.getInfo request.
+    /// </summary>
+    [ApiRequest(ApiMethod.System.GetInfo, HttpVerb.GET, false)]
+    public class SystemGetInfo
+    {
+    }
+}
---
+++ src/NeufboxRestClient/Internal/Models/Responses/SystemGetInfo.cs
@@ -0,0 +1,144 @@
+﻿using System.Xml.Serialization;
+
+namespace NeufboxRestClient.Internal.Models.Responses
+{
+    /// <summary>
+    /// Response to <see cref="Requests.SystemGetInfo"/>.
+    /// </summary>
+    [XmlRoot("system")]
+    public class SystemGetInfo
+    {
+        /// <summary>
+        /// The product ID: $(NB)-$(HARD)-$(HARD_VERSION).
+        /// </summary>
+        [XmlAttribute("product_id")]
+        public string ProductId { get; set; }
+
+        /// <summary>
+        /// The MAC address of the box.
+        /// </summary>
+        [XmlAttribute("mac_addr")]
+        public string MacAddress { get; set; }
+
+        /// <summary>
+        /// The network mode of the box.
+        /// </summary>
+        [XmlAttribute("net_mode")]
+        public NetworkMode NetworkMode { get; set; }
+
+        /// <summary>
+        /// How the box is connected to the Internet.
+        /// </summary>
+        [XmlAttribute("net_infra")]
+        public NetworkInfrastructure NetworkInfrastructure { get; set; }
+
+        /// <summary>
+        /// The uptime of the box (seconds).
+        /// </summary>
+        [XmlAttribute("uptime")]
+        public int Uptime { get; set; }
+
+        /// <summary>
+        /// The box's main firmware version: $(NB)-MAIN-R$(VERSION).
+        /// </summary>
+        [XmlAttribute("version_mainfirmware")]
+        public string VersionMainFirmware { get; set; }
+
+        /// <summary>
+        /// The box's rescue firmware version.
+        /// </summary>
+        [XmlAttribute("version_rescuefirmware")]
+        public string VersionRescueFirmware { get; set; }
+
+        /// <summary>
+        /// The box's bootloader version.
+        /// </summary>
+        [XmlAttribute("version_bootloader")]
+        public string VersionBootloader { get; set; }
+
+        /// <summary>
+        /// The box's DSL driver version.
+        /// </summary>
+        /// <remarks>Unavailable for NB5.</remarks>
+        [XmlAttribute("version_dsldriver")]
+        public string VersionDslDriver { get; set; }
+
+        /// <summary>
+        /// The current date and time.
+        /// </summary>
+        [XmlAttribute("current_datetime")]
+        public string CurrentDatetime { get; set; }
+
+        /// <summary>
+        /// The client reference.
+        /// </summary>
+        [XmlAttribute("refclient")]
+        public string ClientReference { get; set; }
+
+        /// <summary>
+        /// The unique network identifier.
+        /// </summary>
+        [XmlAttribute("idur")]
+        public string NetworkIdentifier { get; set; }
+
+        /// <summary>
+        /// The box's power voltage (mV).
+        /// </summary>
+        [XmlAttribute("alimvoltage")]
+        public int PowerVoltage { get; set; }
+
+        /// <summary>
+        /// The box's temperature (m°C).
+        /// </summary>
+        [XmlAttribute("temperature")]
+        public int Temperature { get; set; }
+
+        /// <summary>
+        /// The serial number.
+        /// </summary>
+        [XmlAttribute("serial_number")]
+        public string SerialNumber { get; set; }
+    }
+
+    /// <summary>
+    /// The known network modes.
+    /// </summary>
+    public enum NetworkMode
+    {
+        /// <summary>
+        /// Box works in router mode.
+        /// </summary>
+        [XmlEnum("router")]
+        Router,
+
+        /// <summary>
+        /// Box works in bridge mode.
+        /// </summary>
+        [XmlEnum("bridge")]
+        Bridge,
+    }
+
+    /// <summary>
+    /// The known network infrastructures.
+    /// </summary>
+    public enum NetworkInfrastructure
+    {
+        /// <summary>
+        /// Infrastructure is ADSL.
+        /// </summary>
+        [XmlEnum("adsl")]
+        ADSL,
+
+        /// <summary>
+        /// Infrastructure is FTTH.
+        /// </summary>
+        [XmlEnum("ftth")]
+        FFTH,
+
+        /// <summary>
+        /// Infrastructure is GPRS.
+        /// </summary>
+        [XmlEnum("gprs")]
+        GPRS,
+    }
+}
