7d55629a7abccf809545f645608a3672471aff86
tbayart <tbayart@nordnet.fr>
2024-03-27 23:45:55 +0100

    Added comments to NetworkInterface

--- src/NeufboxRestClient/Models/NetworkInterface.cs
+++ src/NeufboxRestClient/Models/NetworkInterface.cs
@@ -1,4 +1,9 @@
 ﻿namespace NeufboxRestClient.Models
 {
+    /// <summary>
+    /// Represents a network interface.
+    /// </summary>
+    /// <param name="Name">The interface name.</param>
+    /// <param name="Mac">The interface MAC Address.</param>
     public record NetworkInterface(string Name, string Mac);
 }
