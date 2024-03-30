82e6156320f319e7328fdecda872140e33f1ba2e
tbayart <tbayart@nordnet.fr>
2024-03-30 23:08:53 +0100

    Added missing model LoginResult.cs

---
+++ src/NeufboxRestClient/Models/LoginResult.cs
@@ -0,0 +1,43 @@
+﻿namespace NeufboxRestClient.Models
+{
+    /// <summary>
+    /// Login return codes.
+    /// </summary>
+    public enum LoginResult : int
+    {
+        /// <summary>
+        /// Login successful
+        /// </summary>
+        Success = 200,
+
+        /// <summary>
+        /// Session doen't exists or already authenticated.
+        /// </summary>
+        InvalidSession = 201,
+
+        /// <summary>
+        /// Token expired before beeing validated.
+        /// </summary>
+        Timeout = 202,
+
+        /// <summary>
+        /// Push button not pushed.
+        /// </summary>
+        PushFailed = 203,
+
+        /// <summary>
+        /// Invalid login and/or password.
+        /// </summary>
+        InvalidCredentials = 204,
+
+        /// <summary>
+        /// Authentication disabled.
+        /// </summary>
+        Disabled = 205,
+
+        /// <summary>
+        /// Undocumented error (check logs).
+        /// </summary>
+        Error = 500,
+    }
+}
