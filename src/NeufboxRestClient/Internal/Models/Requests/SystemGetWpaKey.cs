a1a175b0fbee36c01c58d4a26e2d071610c087b6
tbayart <tbayart@nordnet.fr>
2024-04-01 11:53:16 +0200

    Added SystemGetWpaKey request and response models

---
+++ src/NeufboxRestClient/Internal/Models/Requests/SystemGetWpaKey.cs
@@ -0,0 +1,12 @@
+﻿using NeufboxRestClient.Constants;
+
+namespace NeufboxRestClient.Internal.Models.Requests
+{
+    /// <summary>
+    /// system.getInfo request.
+    /// </summary>
+    [ApiRequest(ApiMethod.System.GetWpaKey, HttpVerb.GET)]
+    public class SystemGetWpaKey : AuthenticatedRequest
+    {
+    }
+}
---
+++ src/NeufboxRestClient/Internal/Models/Responses/SystemGetWpaKey.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace NeufboxRestClient.Internal.Models.Responses
+{
+    /// <summary>
+    /// Response to <see cref="Requests.SystemGetWpaKey"/>.
+    /// </summary>
+    [XmlRoot("system")]
+    public class SystemGetWpaKey
+    {
+        /// <summary>
+        /// The validated authentication token.
+        /// </summary>
+        [XmlAttribute("wpa_key")]
+        public string WpaKey { get; set; }
+    }
+}
