f620d9b417a544b9ccc99e584428637c52d15a38
tbayart <tbayart@nordnet.fr>
2024-04-01 11:50:25 +0200

    Added AuthenticatedRequest.cs model to manage requests requiring authentication

---
+++ src/NeufboxRestClient/Internal/Models/Requests/AuthenticatedRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace NeufboxRestClient.Internal.Models.Requests
+{
+    /// <summary>
+    /// The base for authentication required requests.
+    /// </summary>
+    public abstract class AuthenticatedRequest
+    {
+        /// <summary>
+        /// The token provided by <see cref="AuthGetToken"/> request.
+        /// </summary>
+        public string Token { get; set; }
+    }
+}
