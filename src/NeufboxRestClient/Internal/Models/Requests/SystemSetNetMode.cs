6cf7233f6c34527598dec21c1cfbca75ed6bce32
tbayart <tbayart@nordnet.fr>
2024-04-01 18:14:36 +0200

    Added SystemSetNetMode request and response models

---
+++ src/NeufboxRestClient/Internal/Models/Requests/SystemSetNetMode.cs
@@ -0,0 +1,18 @@
+﻿using NeufboxRestClient.Constants;
+using System.Xml.Serialization;
+
+namespace NeufboxRestClient.Internal.Models.Requests
+{
+    /// <summary>
+    /// system.setNetMode request.
+    /// </summary>
+    [ApiRequest(ApiMethod.System.SetNetMode, HttpVerb.POST)]
+    public class SystemSetNetMode
+    {
+        /// <summary>
+        /// The network mode of the box.
+        /// </summary>
+        [XmlAttribute("mode")]
+        public NetworkMode NetworkMode { get; set; }
+    }
+}
---
+++ src/NeufboxRestClient/Internal/Models/Responses/SystemSetNetMode.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml.Serialization;
+
+namespace NeufboxRestClient.Internal.Models.Responses
+{
+    /// <summary>
+    /// Response to <see cref="Requests.SystemSetNetMode"/>.
+    /// </summary>
+    [XmlRoot("system")]
+    public class SystemSetNetMode
+    {
+    }
+}
