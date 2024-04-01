a01ef8faf87beb07a649755a9520d32bc9f8a6ef
tbayart <tbayart@nordnet.fr>
2024-04-01 17:41:59 +0200

    Implemented call to system.reboot

---
+++ src/NeufboxRestClient/Internal/Models/Requests/SystemReboot.cs
@@ -0,0 +1,12 @@
+﻿using NeufboxRestClient.Constants;
+
+namespace NeufboxRestClient.Internal.Models.Requests
+{
+    /// <summary>
+    /// system.reboot request.
+    /// </summary>
+    [ApiRequest(ApiMethod.System.Reboot, HttpVerb.POST)]
+    public class SystemReboot : AuthenticatedRequest
+    {
+    }
+}
---
+++ src/NeufboxRestClient/Internal/Models/Responses/SystemReboot.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml.Serialization;
+
+namespace NeufboxRestClient.Internal.Models.Responses
+{
+    /// <summary>
+    /// Response to <see cref="Requests.SystemReboot"/>.
+    /// </summary>
+    [XmlRoot("system")]
+    public class SystemReboot
+    {
+    }
+}
