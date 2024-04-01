70cc2b112cdace00a06ada8f994126db5e513fc8
tbayart <tbayart@nordnet.fr>
2024-04-01 18:02:42 +0200

    Commit fix : Added SystemGetIfList request and response models

---
+++ src/NeufboxRestClient/Internal/Models/Requests/SystemGetIfList.cs
@@ -0,0 +1,12 @@
+﻿using NeufboxRestClient.Constants;
+
+namespace NeufboxRestClient.Internal.Models.Requests
+{
+    /// <summary>
+    /// system.getIfList request.
+    /// </summary>
+    [ApiRequest(ApiMethod.System.GetIfList, HttpVerb.GET)]
+    public class SystemGetIfList : AuthenticatedRequest
+    {
+    }
+}
---
+++ src/NeufboxRestClient/Internal/Models/Responses/SystemGetIfList.cs
@@ -0,0 +1,18 @@
+﻿using NeufboxRestClient.Models;
+using System.Xml.Serialization;
+
+namespace NeufboxRestClient.Internal.Models.Responses
+{
+    /// <summary>
+    /// Response to <see cref="Requests.SystemGetIfList"/>.
+    /// </summary>
+    [XmlRoot("system")]
+    public class SystemGetIfList
+    {
+        /// <summary>
+        /// The list of network interfaces.
+        /// </summary>
+        [XmlArray("interface")]
+        public NetworkInterface[] Interfaces { get; set; }
+    }
+}
