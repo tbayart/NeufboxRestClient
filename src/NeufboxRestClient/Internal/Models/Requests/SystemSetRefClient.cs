b0917adaf70c46825f95a15c172c36b3b2e999e5
tbayart <tbayart@nordnet.fr>
2024-04-01 18:19:47 +0200

    Added SystemSetRefClient request and response models

---
+++ src/NeufboxRestClient/Internal/Models/Requests/SystemSetRefClient.cs
@@ -0,0 +1,18 @@
+﻿using NeufboxRestClient.Constants;
+using System.Xml.Serialization;
+
+namespace NeufboxRestClient.Internal.Models.Requests
+{
+    /// <summary>
+    /// system.setRefClient request.
+    /// </summary>
+    [ApiRequest(ApiMethod.System.SetRefClient, HttpVerb.POST)]
+    public class SystemSetRefClient : AuthenticatedRequest
+    {
+        /// <summary>
+        /// The client reference to set.
+        /// </summary>
+        [XmlAttribute("refClient")]
+        public string ClientReference { get; set; }
+    }
+}
---
+++ src/NeufboxRestClient/Internal/Models/Responses/SystemSetRefClient.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml.Serialization;
+
+namespace NeufboxRestClient.Internal.Models.Responses
+{
+    /// <summary>
+    /// Response to <see cref="Requests.SystemSetRefClient"/>.
+    /// </summary>
+    [XmlRoot("system")]
+    public class SystemSetRefClient
+    {
+    }
+}
